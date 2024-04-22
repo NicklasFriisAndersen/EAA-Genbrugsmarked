8c6173adbf9c7318cbc4bb02e0b485d831e4898a
vincentlorenzen <vincent.lorenzen@gmail.com>
2024-04-22 09:35:28 +0200

    testuser

--- ServerAPI/Repositories/UserRepository.cs
+++ ServerAPI/Repositories/UserRepository.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using Core.Models;
+using MongoDB.Bson;
+
 namespace ServerAPI.Repositories
+
 {
 	public class UserRepository
 	{
-		public UserRepository()
+		private User testuser = new User()
 		{
-		}
+			//UserID = "1",
+			Username = "testbruger1",
+			Password = "1234567",
+			Email = "test@gmail.com",
+			Role = "",
+			Orders = new List<Order>(),
+			Listings = new List<Listing>()
+		};
 	}
+	
+
 }
 
