bcabcdb20cf81b6c8f2872653c77e28fb27386d1
vincentlorenzen <vincent.lorenzen@gmail.com>
2024-04-23 12:27:49 +0200

    tilføjet id til listing

--- Core/Models/Listing.cs
+++ Core/Models/Listing.cs
@@ -5,6 +5,10 @@ namespace Core.Models;
 
 public class Listing
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("_id")]
+    public string? Id { get; set; }
     public string Title { get; set; }
     
     public string Description { get; set; } = "";
