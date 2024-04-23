464c030a7425186666a3e5730552439a51ecd520
NicklasEAA <eaa23nrfa@students.eaaa.dk>
2024-04-23 11:39:55 +0200

    create user

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -10,6 +10,8 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddSingleton<IListingRepository, ListingRepository>();
+        builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
 
         builder.Services.AddCors(options =>
         {
--- ServerAPI/Repositories/UserRepository.cs
+++ ServerAPI/Repositories/UserRepository.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using Core.Models;
 using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace ServerAPI.Repositories
 
 {
-	public class UserRepository
-	{
-		private User testuser = new User()
-		{
-			Id = ObjectId.GenerateNewId(),
-			Username = "testbruger1",
-			Password = "1234567",
-			Email = "test@gmail.com",
-			Role = "",
-			Orders = new List<Order>(),
-			Listings = new List<Listing>()
-		};
-	}
+	public class UserRepository : IUserRepository
+    {
+        private string connectionString = "mongodb+srv://gruppe3mini:gruppe3projekt@genbrugshjemmeside.l92667j.mongodb.net/?retryWrites=true&w=majority&appName=Genbrugshjemmeside";
+
+        IMongoClient mongoClient;
+
+        IMongoDatabase database;
+
+        IMongoCollection<User> collection;
+
+
+        public UserRepository()
+        {
+            mongoClient = new MongoClient(connectionString);
+
+            database = mongoClient.GetDatabase("Genbrugsmarked");
+
+            collection = database.GetCollection<User>("User");
+        }
+
+        public void insertOneUser(User user)
+        {
+            collection.InsertOne(user);
+        }
+    }
 	
 
 	 
