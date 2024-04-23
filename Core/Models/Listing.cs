860a3affa9bd0f532592409965958f5593849dfb
NicklasEAA <eaa23nrfa@students.eaaa.dk>
2024-04-23 11:05:52 +0200

    Listings working

--- Core/Models/Listing.cs
+++ Core/Models/Listing.cs
@@ -5,22 +5,22 @@ namespace Core.Models;
 public class Listing
 {
     public ObjectId Id { get; set; }
+
+    public string Title { get; set; } = "";
     
-    public string Title { get; set; }
-    
-    public string Description { get; set; }
-    
-    public string Status { get; set; }
-    
-    public decimal Price { get; set; }
-    
-    public string Category { get; set; }
-    
+    public string Description { get; set; } = "";
+
+    public string Status { get; set; } = "Available";
+
+    public decimal Price { get; set; } = 1;
+
+    public string Category { get; set; } = "";
+
     public DateTime DatePosted { get; set; }
     
-    public Location Location { get; set; }
+    public Location? Location { get; set; }
     
-    public User User { get; set; }
+    public User? User { get; set; }
 
 }
 
